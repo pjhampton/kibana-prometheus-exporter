777af8490e2b30f03a0e3a6a79870fb3b995b621
PJ Hampton <pjhampton@protonmail.com>
2018-09-02 12:03:14 +0100

    [pjh] fix formatter.

--- server/routes/formatter.js
+++ server/routes/formatter.js
@@ -3,17 +3,17 @@ export default function (info) {
     let metrics = {};
 
     metrics['kibana_status'] = convert_state_to_number(info.status.overall.state);
-    metrics['kibana_millis_uptime'] = info.metrics.uptime_in_millis;
-    metrics['kibana_heap_max_in_bytes'] = info.metrics.process.mem.heap_max_in_bytes || 0;
-    metrics['kibana_heap_used_in_bytes'] = info.metrics.process.mem.heap_used_in_bytes || 0;
+    metrics['kibana_millis_uptime'] = info.metrics.process.uptime_in_millis;
+    metrics['kibana_heap_max_in_bytes'] = info.metrics.process.memory.heap.total_in_bytes || 0;
+    metrics['kibana_heap_used_in_bytes'] = info.metrics.process.memory.heap.used_in_bytes || 0;
     metrics['kibana_response_average'] = info.metrics.response_times.avg_in_millis || 0;
     metrics['kibana_response_max'] = info.metrics.response_times.max_in_millis || 0;
     metrics['kibana_concurrent_connections'] = info.metrics.concurrent_connections || 0;
     metrics['kibana_requests_total'] = info.metrics.requests.total || 0;
-    metrics['kibana_requests_disconnects'] = info.metrics.disconnects || 0;
-    metrics['kibana_os_cpu_load_average{interval="1m"}'] = info.metrics.os.cpu.load_average['1m'] || 0;
-    metrics['kibana_os_cpu_load_average{interval="5m"}'] = info.metrics.os.cpu.load_average['5m'] || 0;
-    metrics['kibana_os_cpu_load_average{interval="15m"}'] = info.metrics.os.cpu.load_average['15m'] || 0;
+    metrics['kibana_requests_disconnects'] = info.metrics.requests.disconnects || 0;
+    metrics['kibana_os_cpu_load_average{interval="1m"}'] = info.metrics.os.load['1m'] || 0;
+    metrics['kibana_os_cpu_load_average{interval="5m"}'] = info.metrics.os.load['5m'] || 0;
+    metrics['kibana_os_cpu_load_average{interval="15m"}'] = info.metrics.os.load['15m'] || 0;
 
     for(var key in info.status.statuses) {
         let plugin_name = info.status.statuses[key]['id'].split(/:|@/)[1];
