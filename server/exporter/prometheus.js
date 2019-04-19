ccb2f3388f66657b6094b7c679b620a0db5b7bcf
PJ Hampton <pjhampton@protonmail.com>
2019-04-19 15:48:04 +0100

    remove ssl stuff. add reject unauthorized

--- server/exporter/prometheus.js
+++ server/exporter/prometheus.js
@@ -12,20 +12,13 @@ export default function (server) {
   const path = config.get('server').basePath.toString();
   const user = config.get('kibana-prometheus-exporter.user');
   const pass = config.get('kibana-prometheus-exporter.pass');
-  const sslEnabled = config.get('server.ssl.enabled');
-  const sslCert = config.get('server.ssl.certificate');
-  const sslKey = config.get('server.ssl.key');
 
   const request = {
     uri: makeUrl(server.info.uri, path),
     auth: { 'user': user, 'pass': pass },
+    rejectUnauthorized: false,
     json: true
   };
-
-  if (sslEnabled && sslCert && sslKey) {
-    request['cert'] = fs.readFileSync(sslCert);
-    request['key'] = fs.readFileSync(sslKey);
-  }
 
   server.route({
     path: config.get('kibana-prometheus-exporter.path'),
