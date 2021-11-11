1e181d0af234ff6cc724688b9b4ffe8ceb2e2ce6
Pete Hampton <peter.hampton@elastic.co>
2021-11-11 14:53:37 +0000

    update the formatter

--- common/formatter.ts
+++ common/formatter.ts
@@ -2,47 +2,43 @@
 export default function (info : any) {
     const metrics = new Map<string, number>();
 
-    metrics.set('kibana_status', convert_state_to_number(info.status.overall.state));
+    metrics.set('kibana_status', convertStateToScalar(info.status.overall.level));
     metrics.set('kibana_millis_uptime', info.metrics.process.uptime_in_millis);
     metrics.set('kibana_heap_max_in_bytes', info.metrics.process.memory.heap.size_limit || 0);
     metrics.set('kibana_heap_used_in_bytes', info.metrics.process.memory.heap.used_in_bytes || 0);
     metrics.set('kibana_response_average', info.metrics.response_times.avg_in_millis || 0);
     metrics.set('kibana_response_max', info.metrics.response_times.max_in_millis || 0);
-    metrics.set('kibana_concurrent_connections', info.metrics.concurrent_connections || 0);
+    metrics.set('kibana_concurrent_connections', info.metrics.concurrent_connections || '0');
     metrics.set('kibana_requests_total', info.metrics.requests.total || 0);
     metrics.set('kibana_requests_disconnects', info.metrics.requests.disconnects || 0);
     metrics.set('kibana_os_load1', info.metrics.os.load['1m'] || 0);
     metrics.set('kibana_os_load5', info.metrics.os.load['5m'] || 0);
     metrics.set('kibana_os_load15', info.metrics.os.load['15m'] || 0);
-
-    for(var key in info.status.statuses) {
-        let plugin_name = info.status.statuses[key]['id'].split(/:|@/)[1];
-        
-        if (plugin_name != null) {
-            plugin_name = plugin_name.replace(/-/g, '_')
-        }
-        
-        metrics.set('kibana_plugin_' + plugin_name, convert_state_to_number(info.status.statuses[key]['state']));
+    metrics.set('kibana_os_mem_bytes_total', info.metrics.os.memory['total_in_bytes'] || 0);
+    metrics.set('kibana_os_mem_bytes_free', info.metrics.os.memory['free_in_bytes'] || 0);
+    metrics.set('kibana_os_mem_bytes_used', info.metrics.os.memory['used_in_bytes'] || 0);
+    metrics.set('kibana_os_uptime', info.metrics.os['uptime_in_millis'] || 0);
+    
+    for(var key in info.status.plugins) {
+        metrics.set('kibana_plugin_' + key, convertStateToScalar(info.status.plugins[key]['level']));
     }
 
-    const formatted = prometheus_style_formatter(metrics);
-
-    return formatted;
+    return prometheusStyleFormatter(metrics)
 }
 
-const convert_state_to_number = (state: string): number => {
-    if (state == 'green') {
+const convertStateToScalar = (state: string): number => {
+    if (state == 'available') {
         return 1;
-    }
-    else if (state == 'yellow') {
+    } else if (state == 'degraded') {
         return 0.5;
-    }
-    else {
+    } else if (state == 'unavailable') {
         return 0;
+    } else {
+        return -1;
     }
 }
 
-const prometheus_style_formatter = (metrics: Map<string, number>): string => {
+const prometheusStyleFormatter = (metrics: Map<string, number>): string => {
     let prometheus_style = '';
 
     for (let [key, value] of metrics) {
