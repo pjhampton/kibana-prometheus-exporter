2e330f94195034ee7cbd926f88b5b1f246948ab0
Pete Hampton <peter.hampton@elastic.co>
2022-01-28 17:50:35 +0000

    Sanitize url before request.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,8 @@
-import { IRouter } from '../../../../src/core/server';
+import { IRouter } from '../../../../src/core/server'
 import formatter  from '../../common/formatter'
-import axios from 'axios';
-import agent from '../helpers/server_client';
+import axios from 'axios'
+import { sanitizeUrl } from '@braintree/sanitize-url'
+import agent from '../helpers/server_client'
 
 export function defineRoutes(router: IRouter, statsURI: string) {
   router.get(
@@ -14,7 +15,7 @@ export function defineRoutes(router: IRouter, statsURI: string) {
       let reqProto = request.url.protocol || 'http:';
       let reqHost = request.url.host?.replace(/:\d+/, '') || '127.0.0.1';
       let reqPort = request.url.port || 5601;
-      let reqUrl = `${reqProto}//${reqHost}:${reqPort}${statsURI}`;
+      let reqUrl = sanitizeUrl(`${reqProto}//${reqHost}:${reqPort}${statsURI}`);
 
       if (request.headers !== undefined
           && request.headers.authorization !== undefined) {
